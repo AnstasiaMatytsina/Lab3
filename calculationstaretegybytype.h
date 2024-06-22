3ba23b6e615263845354b3ce13f06c462e2399cc
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2024-06-22 23:29:59 +0700

    Класс CalculationStaretegyByType
    Написала стратегию подсчета размера по типам файлов. Хранить будем в map( тип, размер). Тип получаем с помощью метода suffix() у QFileInfo и также записываем в вектор

--- calculationstaretegybytype.h
+++ calculationstaretegybytype.h
@@ -1,12 +1,33 @@
 #ifndef CALCULATIONSTARETEGYBYTYPE_H
 #define CALCULATIONSTARETEGYBYTYPE_H
 #include <calculationstrategy.h>
+#include <QFileInfo>
+#include <QDirIterator>
+#include <QVector>
 
 class CalculationStaretegyByType : public CalculationStrategy
 {
 public:
-    CalculationStaretegyByType();
-    QVector<CalculatedStrategyData> calculation(QString path) override;
+    CalculationStaretegyByType() = default;
+    QVector<CalculatedStrategyData> calculation(QString path) override {
+        QFileInfo pathValue(path);
+        if (pathValue.isDir() != 1) {
+            throw std::runtime_error("Path value is not directory");
+        }
+        QDirIterator dirIterator(path, QDir::Files, QDirIterator::Subdirectories);
+        std::map<QString, qint64> typesInfo;
+        while(dirIterator.hasNext())
+        {
+           QString filePath = dirIterator.next();
+           QFileInfo file(filePath);
+           typesInfo[file.suffix()] += file.size();
+        }
+        QVector<CalculatedStrategyData> result;
+        for (auto typeInfo : typesInfo) {
+            result.push_back(CalculatedStrategyData(typeInfo.first, typeInfo.second));
+        }
+        return result;
+    }
 };
 
 #endif // CALCULATIONSTARETEGYBYTYPE_H
