46636305b352f6ac5f6304109478cfa786e02b07
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2024-06-22 21:00:48 +0700

    Класс  CalculationStrategyByFolder
    Написала функцию calculation, которая будет возвращать вектор размеров и подписей выбранных файлов. Проходим по  папке, заходя в каждую директорию и суммируя размер

--- calculationstrategybyfolder.h
+++ calculationstrategybyfolder.h
@@ -1,12 +1,36 @@
 #ifndef CALCULATIONSTRATEGYBYFOLDER_H
 #define CALCULATIONSTRATEGYBYFOLDER_H
 #include <calculationstrategy.h>
+#include <QFileInfo>
+#include <QDirIterator>
+#include <QVector>
 
 class CalculationStrategyByFolder : public CalculationStrategy
 {
 public:
     CalculationStrategyByFolder() = default;
-    QVector<CalculatedStrategyData> calculation(QString path) override;
+    QVector<CalculatedStrategyData> calculation(QString path) override {
+        QFileInfo pathValue(path);
+        if (pathValue.isDir() != 1) {
+            throw std::runtime_error("Path value is not directory");
+        }
+        QDirIterator dirIterator(path, QDir::Dirs | QDir::NoDotAndDotDot);
+        QVector<CalculatedStrategyData> result;
+        while(dirIterator.hasNext())
+        {
+           QString dirPath = dirIterator.next();
+           QString dirName = dirIterator.fileName();
+           qint64 dirSize = 0;
+           QDirIterator subDirIterator(dirPath, QDir::Files, QDirIterator::Subdirectories);
+           while(subDirIterator.hasNext()) {
+               QString subFilePath = subDirIterator.next();
+               QFileInfo file(subFilePath);
+               dirSize += file.size();
+           }
+           result.push_back(CalculatedStrategyData(dirName, dirSize));
+        }
+        return result;
+    }
 };
 
 #endif // CALCULATIONSTRATEGYBYFOLDER_H
